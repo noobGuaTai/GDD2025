9dacad1121f219c1f3c1330dcb8eeb5d1cd82fce
noobGuaTai <75893628+noobGuaTai@users.noreply.github.com>
2025-09-20 10:44:07 +0800

    Init

---
+++ Source/GDD2025/GDD2025.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GDD2025.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GDD2025, "GDD2025" );
